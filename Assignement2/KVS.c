4f2b8e5ee8dbf5aaf67df474a7f07451e011c3ee
William Nguyen <WDDnguyen@gmail.com>
2017-03-03 20:40:02 -0500

    fixed reading and add read all memory

--- Assignement2/KVS.c
+++ Assignement2/KVS.c
@@ -2,7 +2,7 @@
 
 int fd;
  
-size_t currentRecord = 0;
+int currentRecord = 0;
 //size_t keySize = 32;
 //size_t valueSize = 256; 
 //size_t sizeOfRecord = 288;
@@ -48,20 +48,23 @@ strcpy(concatenatedString,key);
 strcat(concatenatedString,value);
 
 size_t offset = sizeOfRecord * currentRecord;
+//printf("CURRENT RECORD TO WRITE IN : %d\n",currentRecord);
 //printf("WRITTING INTO SHARED MEMORY\n"); 
 memcpy(addressOfSharedMemory + offset, key , keySize);
 memcpy(addressOfSharedMemory + offset + keySize, value , valueSize); 
 currentRecord = currentRecord + 1;
 
+
 }
 //got to change later
+//POSSIBLE LEAK
 char* kv_store_read(char *key){
 	char* duplicated;
 	int i = 0;
 	size_t offset = 0;
 	// duplicate string 
 	for (i = 0; i < currentRecord ; i++){
-		offset = offset + sizeOfRecord * i;
+		offset = sizeOfRecord * i;
 		if( memcmp( addressOfSharedMemory + offset , key, strlen(key))  == 0){
 			printf("SAME KEY at record : %d\n", i);
 				
@@ -77,6 +80,39 @@ char* kv_store_read(char *key){
 
 }
 
+// POSSIBLE LEAK
+char ** kv_store_read_all(char *key){
+	char** allValuesOfKey = malloc(sizeof(256 * maximumOfRecords));
+	char* value;
+
+	int index = 0;
+	int i = 0;
+	size_t offset = 0;
+	//printf("CURRENT RECORD : %d\n", currentRecord);
+	for ( i = 0; i < currentRecord ; i++){
+	offset = sizeOfRecord * i;
+	//printf("CURRENT INDEX I : %d\n", i);
+	printf("PRINTING OFFSET VALUE : %zu\n", offset);
+ 
+	if (memcmp( addressOfSharedMemory + offset, key, strlen(key)) == 0){
+		printf("SAME KEY AT RECORD : %d\n", i);
+		value = addressOfSharedMemory + offset + keySize;
+		allValuesOfKey[index] = value;
+		index = index + 1;
+		printf("ADDED INTO ALL VALUES\n"); 
+		
+	}
+
+	}
+	if (allValuesOfKey != NULL){
+	return allValuesOfKey;
+	}else {
+	return NULL;
+	}
+		
+	//return NULL; // if no value of that key
+}
+
 void displayAllSharedMemory(){
 
 struct stat s;
@@ -88,11 +124,11 @@ write(STDOUT_FILENO, addressOfSharedMemory, s.st_size);
 
 }
 
-
 int main(int argc, char *argv[]){
 
 KVpair pair1;
 KVpair pair2;
+KVpair pair3;
 
 strcpy(pair1.key,"Catherine");
 strcpy(pair1.value,"Portal");
@@ -100,19 +136,37 @@ strcpy(pair1.value,"Portal");
 strcpy(pair2.key,"cake");
 strcpy(pair2.value, "IsALie");
 
+strcpy(pair3.key,"cake");
+strcpy(pair3.value, "NOT A LIE");
+
+
 kv_store_create(SharedMemoryName);
 setSharedMemoryAddress();
 kv_store_write(pair1.key , pair1.value);
 kv_store_write(pair2.key , pair2.value);
+kv_store_write(pair3.key, pair3.value);
+
+
 
 char* duplicated1;
 char* duplicated2;
+char* duplicated3;
 
-duplicated1 = kv_store_read(pair2.key);
-duplicated2 = kv_store_read(pair1.key);
+//duplicated1 = kv_store_read(pair2.key);
+//duplicated2 = kv_store_read(pair1.key);
+//duplicated3 = kv_store_read(pair3.key);
 
-printf("USING KEY : %s , OBTAINED VALUE : %s\n",pair1.key, duplicated1);
-printf("USING KEY : %s , OBTAINED VALUE : %s\n",pair2.key, duplicated2);
+//printf("USING KEY : %s , OBTAINED VALUE : %s\n",pair1.key, duplicated1);
+//printf("USING KEY : %s , OBTAINED VALUE : %s\n",pair2.key, duplicated2);
+//printf("USING KEY : %s , OBTAINED VALUE : %s\n",pair3.key, duplicated3);
+
+char** sameKey;
+int i = 0;
+sameKey = kv_store_read_all(pair3.key);
+for ( i = 0 ; i < 3; i++){
+printf("USING KEY : %s, VALUE FOR A KEY IS : %s\n", pair3.key, sameKey[i]); 
+
+}
 
 //displayAllSharedMemory();
 
