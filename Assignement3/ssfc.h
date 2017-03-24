12a9d4cc44a3fb9cbee17eafdf7420e585f1221d
William Nguyen <WDDnguyen@gmail.com>
2017-03-23 22:42:14 -0400

    added disk and initialize super/FBM/WM

--- Assignement3/ssfc.h
+++ Assignement3/ssfc.h
@@ -1,13 +1,16 @@
+#include "disk_emu.h"
+
 // size of components
 #define blockSize 1024
 #define sizeOfPointer 4
-#define numberOfBlocks 2000
-#define numberOfInodes 200
-#define numberOfInodeblocks 13
+#define numberOfBlocks 200
+#define numberOfInodes 14
+#define numberOfInodeblocks 1
 #define sizeOfInode 64
 
+
 //super block 
-#define magic  0xACBD0005
+#define magicValue 0xACBD0005
 #define sizeOfSuperBlockField 4 
 #define SBBlockSize 1
 
@@ -17,14 +20,54 @@
 #define WMBlockSize 1
 
 
+// non standard inode 
+// size field  total number of bytes 
+// no need to know about indirect 
+// these block contains data
+
+//File has lots of inodes 
+// for j node   size = size of the file 
+// set size = -1 to be blank
+
+
+typedef struct {
+	int size; 
+	int direct[14];
+	int indirect;
+} inode_t;
+
 // root is a jnode
 // have to add shadow jnode later
-typedef struct {
-uint64_t magic;
-uint64_t block_size;
-uint64_t fs_size;
-uint64_t numberOfInodes;
+// need to fill to get to 1024 or  copy memory to block_t then pass that to the disk * calloc
 
-uint64_t root;
+typedef struct {
+	
+unsigned char magic[4];
+
+int block_size;
+int fs_size;
+int Inodes;
+inode_t root;
+inode_t shadow[4];
+int lastShadow;
 
 } superblock_t;
+
+
+// can use uint8_t 
+typedef struct{
+	unsigned char bytes[blockSize];
+}block_t;
+
+/*
+typedef struct {
+    int free;
+    int inode;
+    int rwptr;
+} fileDescriptor_t;
+ 
+typedef struct {
+    char name[16];
+    int inode;
+} directoryEntry_t;
+*/
--- Assignement3/ssfs.c
+++ Assignement3/ssfs.c
@@ -1,33 +1,150 @@
 // In progress 
 #include "ssfc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <fcntl.h>
+
+
+#include "disk_emu.h"
 
 superblock_t sb;
-int fbm[bitsInABlock];
-int wm[bitsInABlock];
+block_t fbm;
+block_t wm;
 
+// to do 
+int inodeFile[numberOfInodes];
+
+// File descriptor table to do 
+//fileDescriptor_t fdt[numberOfInodes];
 
 void initializeSuperBlock(){
-	sb.magic = magic;
-	sb.blockSize = blockSize;
+	
+	inode_t root;
+	int i;
+	// need to implement shadow later 
+	
+	// need to adjust root size 
+	root.size = 1000;
+	for( i = 0 ; i < numberOfInodes ; i++){
+		root.direct[i] = 0;
+	}
+	
+	sb.magic[0] = 0xAC;
+	sb.magic[1] = 0xBD;
+	sb.magic[2] = 0x00;
+	sb.magic[3] = 0x05;
+	sb.block_size = blockSize;
 	sb.fs_size = blockSize * numberOfBlocks; 
-	sb.numberOfInodes = numberOfInodes;
-	// probably need to modify root and add j nodes.
-	sb.root = 0;
+	sb.Inodes = numberOfInodes;
+	sb.root = root;
 } 
 
 void initializeFBM(){
-	for (int i = 0 ; i < bitsInABlock; i++){
-		fbm[i] = 1;
+	int i;
+	for (i = 0 ; i < blockSize; i++){
+		fbm.bytes[i] = 0xFF;
 	}
 }
 
 void initializeWM(){
-	// might need to change value for initialization
-	for(int i = 0; i < bitsInABlock; i++){
-		wm[i] = 1;
+	int i;
+	for(i = 0; i < blockSize; i++){
+		wm.bytes[i] = 0xFF;
 	}
 }
 
+int FBMGetFreeBit(){
+	int i;
+	int k;
+	for (i = 0; i < blockSize; i++){
+		for (k = 0; k < 8 ; k++){
+			if (fbm.bytes[i] & (1 << k) != 0x00){
+				
+				printf("Found a free bit at : %d\n", i);
+				// set the bit to 0 
+				fbm.bytes[i] = fbm.bytes[i] ^ (1 << k);
+				return (i * 8 + k); 
+			} 
+		}
+	}
+	
+	printf("No more free bits left \n");
+	return -1; 
+}
+/*
+void FBMSetBit(int index){
+	fbm.bytes[index] = 0x00;
+}
+
+void FBMClearBit(int index){
+	fbm.bytes[index] = 0xFF;
+}
+*/
+
+// initialize file directory  and set all values to free, rwptr to 0 and  no inode values 
+
+/*
+void initializeFileDescriptorTable() {
+	int i;
+	fileDescriptor_t* tempfd = malloc(sizeof(fileDescriptor_t));
+	tempfd->free = 1;
+	tempfd->rwptr = 0;
+	tempfd->inode = -1;
+	for (i = 0; i < numberOfInodes; i++){
+		memcpy(&fdt[i], tempfd, sizeof(fileDescriptor_t));
+	}
+}
+
+*/
+void mkssfs(){
+	initializeFBM();
+	initializeWM();
+
+	// block_t fbm, wm 
+	// loop bytes[i] = 0xFF 
+	// write_block(1,1 &fbm)
+	
+	
+}
+
+/* 
+ssfc_fopen(char* name ){
+	access rootDirectory
+   // finish without the commit then do commit	
+}
+}
+*/
+
+int main(){
+	
+	mkssfs();
+	int i;
+	int k;
+	for(i = 0; i < blockSize; i++){
+		for(k = 0 ; k < 8 ;k++){
+		printf("%u ", !!(fbm.bytes[i] & (1 << k)));
+		
+		}
+		printf("\n");
+		
+	}
+
+	for(i = 0; i < blockSize; i++){
+		for(k = 0 ; k < 8 ;k++){
+		printf("%u", !!(wm.bytes[i] & (1 << k)));
+		
+		}
+		printf("\n");
+		
+	}
+	
+	return 0;
+}
 
 
 
